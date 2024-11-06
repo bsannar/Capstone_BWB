c6a3db6b8cf61275a6daffcaaf1bdb26e8cd3986
bsannar <bsannartheyounger@gmail.com>
2024-11-06 09:28:41 -0700

    fixes the save and load functions for the new divided variable class structure

--- BWB_GUI/config_saver.py
+++ BWB_GUI/config_saver.py
@@ -1,5 +1,5 @@
 # config_saver.py
-def class_to_csv(bwb_configurations_list,file_name):
+def class_to_csv(bwb_configurations_list, file_name):
     import csv
 
     # Check if there is at least one configuration in the list to define the headers
@@ -7,63 +7,35 @@ def class_to_csv(bwb_configurations_list,file_name):
         print("No configurations to save.")
         return
 
-    # Use vars() on the first item to get the attribute names dynamically
-    headers = list(vars(bwb_configurations_list[0]).keys())
+    # Get headers by combining attributes of independentVars and dependentVars
+    sample_bwb = bwb_configurations_list[0]
+    independent_headers = [f"independent_{key}" for key in vars(sample_bwb.independentVars)]
+    dependent_headers = [f"dependent_{key}" for key in vars(sample_bwb.dependentVars)]
+    headers = independent_headers + dependent_headers
 
     # Open or create the CSV file to save the configurations
-
     with open(file_name, mode='w', newline='') as file:
         writer = csv.writer(file)
 
         # Write the header row
         writer.writerow(headers)
 
-
         # Write each configuration's values
         for bwb in bwb_configurations_list:
-            # Get the values of all attributes dynamically using vars()
-            values = list(vars(bwb).values())
+            # Get values of independentVars and dependentVars
+            independent_values = list(vars(bwb.independentVars).values())
+            dependent_values = list(vars(bwb.dependentVars).values())
+            values = independent_values + dependent_values
             writer.writerow(values)
 
-
-# # config_loader.py
-# def csv_to_class(file_name):
-#     import csv
-#     from bwb_class import BWB  # Import the bwb class
-
-#     instances = []
-
-#     # Open the CSV file to read the configurations
-#     with open(file_name, mode='r', newline='') as file:
-#         reader = csv.reader(file)
-
-#         # Read the header row to get attribute names
-#         headers = next(reader)
-
-#         # Read each row and create an instance of BWB
-#         for row in reader:
-#             # Create an instance of the BWB class without calling __init__
-#             instance = BWB.__new__(BWB)
-
-#             # Set the attributes dynamically based on headers and values
-#             for attr, value in zip(headers, row):
-#                 setattr(instance, attr, value)
-
-#             # Append the instance to the list
-#             instances.append(instance)
-
-#     return instances
 
 
 # config_loader.py
 def csv_to_class(file_name):
     import csv
-    from bwb_class import BWB  # Import the BWB class
+    from bwb_class import Bwb, BwbJetIndependentVars, BwbDependentVars
 
     instances = []
-
-    # Get the list of attributes from the BWB class (e.g., by checking its __init__ method or defined attributes)
-    expected_headers = [attr for attr in dir(BWB) if not callable(getattr(BWB, attr)) and not attr.startswith("__")]
 
     # Open the CSV file to read the configurations
     with open(file_name, mode='r', newline='') as file:
@@ -72,22 +44,32 @@ def csv_to_class(file_name):
         # Read the header row to get attribute names
         headers = next(reader)
 
-        # Check if headers match expected attributes
-        if set(headers) != set(expected_headers):
-            print("Error: CSV headers do not match the BWB class attributes.")
-            return None  # Exit function if headers do not match
+        # Identify attributes for independentVars and dependentVars based on headers
+        independent_attrs = [header for header in headers if header.startswith("independent_")]
+        dependent_attrs = [header for header in headers if header.startswith("dependent_")]
 
-        # Read each row and create an instance of BWB
+        # Read each row and create an instance of Bwb
         for row in reader:
-            # Create an instance of the BWB class without calling __init__
-            instance = BWB.__new__(BWB)
+            # Create instances of independentVars and dependentVars
+            independent_instance = BwbJetIndependentVars.__new__(BwbJetIndependentVars)
+            dependent_instance = BwbDependentVars.__new__(BwbDependentVars)
 
-            # Set the attributes dynamically based on headers and values
-            for attr, value in zip(headers, row):
-                setattr(instance, attr, value)
+            # Set attributes for independentVars
+            for attr, value in zip(independent_attrs, row):
+                actual_attr = attr.replace("independent_", "")  # Remove prefix
+                setattr(independent_instance, actual_attr, value)
+
+            # Set attributes for dependentVars
+            for attr, value in zip(dependent_attrs, row[len(independent_attrs):]):
+                actual_attr = attr.replace("dependent_", "")  # Remove prefix
+                setattr(dependent_instance, actual_attr, value)
+
+            # Create the main Bwb instance and assign the nested instances
+            instance = Bwb(independent_instance, dependent_instance)
 
             # Append the instance to the list
             instances.append(instance)
 
     return instances
+
 
