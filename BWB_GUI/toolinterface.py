e89575f4d18384007b63e72a3ff5590e2d2c0ef8
Tage Burnett <tageburnett@gmail.com>
2025-02-05 08:37:46 -0700

    Added toolinterface.py to repo

---
+++ BWB_GUI/toolinterface.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+class ToolInterface(ABC):
+    @abstractmethod
+    def pull_geometry_from_tool(self):
+        pass
+
+    @abstractmethod
+    def push_geometry_to_tool(self, geometry_dict: dict):
+        pass
+
+    @abstractmethod
+    def pull_mission_inputs_from_tool(self):
+        pass
+
+    @abstractmethod
+    def push_mission_inputs_to_tool(self, mission_dict: dict):
+        pass
+
+    @abstractmethod
+    def close_interface(self):
+        pass
+
+    @abstractmethod
+    def calculate_dry_weight(self):
+        pass
+
+    @abstractmethod
+    def calculate_lift_over_drag(self):
+        pass
+
+    @abstractmethod
+    def calculate_f35s_refueled(self):
+        pass
+
+    @abstractmethod
+    def calculate_max_range(self):
+        pass
